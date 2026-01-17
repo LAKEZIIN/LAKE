0e32d373fd62550ec3987d5ae6de23f39998fc89
LAKEZIIN <vitormendezrp@icloud.com>
2026-01-16 23:09:09 -0300

    Delete festival directory

--- festival/festival.js
+++
@@ -1,8 +0,0 @@
-import { loadFestivalSongs } from "./api/festivalSongs.js";
-import { loadFestivalCosmetics } from "./api/festivalCosmetics.js";
-import { loadFestivalPasses } from "./api/festivalPasses.js";
-import { openSongModal } from "./components/modal.js";
-
-loadFestivalSongs(openSongModal);
-loadFestivalCosmetics();
-loadFestivalPasses();
